e271af5b0f170e5519c90f180f7be54517245990
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-01 14:51:30 +0200

    add for loop and if conditional

--- assests/js/main.js
+++ assests/js/main.js
@@ -10,9 +10,40 @@ let multiples_of_five;
 // creo una variabile per il messaggio di output per i multipli di 3 e di 5
 let multiples_of_both;
 
+// creo una variabile message
+let message
+
 // 2 --> Raccolta dei dati
 // dichiaro le variabili
+multiples_of_three = "Fizz";
+multiples_of_five = "Buzz";
+
+// stampo le variabili
+console.log(multiples_of_three, multiples_of_five);
 
 // 3 --> Elaborazione codice 
+// creazione del loop
+for (i = 1; i <= 100; i++) {
 
-// 4 --> Stamapare output
+    // inserimento del conditional block
+    // verifico se i numeri sono multipli di 3 e di 5
+    if (i % 3 === 0 && i % 5 === 0) { 
+        message = `${multiples_of_three} ${multiples_of_five}`
+
+        // verifico se sono multipli di 5
+    } else if (i % 5 === 0) {
+        message = multiples_of_five
+
+        // verifico se sono multipli di 3
+    } else if (i % 3 === 0) {
+        message = multiples_of_three
+
+        // altrimenti non sono multipli di nessuno dei due
+    } else {
+        message = i
+    }
+
+    // 4 --> Stampare output
+    console.log(message);
+}
+
