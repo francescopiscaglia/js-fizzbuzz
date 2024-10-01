a90e1a76163c258a016daf19ee7ce7b8ab1e663c
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-01 14:22:15 +0200

    add variables

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,8 +1,17 @@
 console.log("Hello world");
 
 // 1 --> Preparazione
+// creo una variabile per il messaggio di output per i multipli di 3
+let multiples_of_three;
+
+// creo una variabile per il messaggio di output per i multipli di 5
+let multiples_of_five;
+
+// creo una variabile per il messaggio di output per i multipli di 3 e di 5
+let multiples_of_both;
 
 // 2 --> Raccolta dei dati
+// dichiaro le variabili
 
 // 3 --> Elaborazione codice 
 
